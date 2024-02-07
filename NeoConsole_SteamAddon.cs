3a910fcb34a586034cd9ec5869ac309f4b736a25
Nylux <nylux42@gmail.com>
2024-02-07 02:40:51 +0100

    Updated for latest MSL release

--- NeoConsole_SteamAddon.cs
+++ NeoConsole_SteamAddon.cs
@@ -13,17 +13,17 @@ public class NeoConsole_SteamAddon : Mod
     public override string Author => "Nylux";
     public override string Name => "NeoConsole Addon - Steam";
     public override string Description => "Adds commands unique to steam. Requires NeoConsole.";
-    public override string Version => "1.1.0";
+    public override string Version => "1.1.1";
 
     public override void PatchMod()
     {
         // Adding the scripts to the game
-        ModLoader.AddFunction(ModFiles.GetCode("scr_neoconsole_clear_steam.gml"), "scr_neoconsole_clear_steam");
-        ModLoader.AddFunction(ModFiles.GetCode("scr_neoconsole_unlock_steam.gml"), "scr_neoconsole_unlock_steam");
+        Msl.AddFunction(ModFiles.GetCode("scr_neoconsole_clear_steam.gml"), "scr_neoconsole_clear_steam");
+        Msl.AddFunction(ModFiles.GetCode("scr_neoconsole_unlock_steam.gml"), "scr_neoconsole_unlock_steam");
         
         // Injecting scr_neoconsole_init to add the commands and their help to the console.
-        ModLoader.InsertGMLString(ModFiles.GetCode("commandsMap.gml"), "scr_neoconsole_init", 13);
-        ModLoader.InsertGMLString(ModFiles.GetCode("helpSyntaxMap.gml"), "scr_neoconsole_init", 77);
-        ModLoader.InsertGMLString(ModFiles.GetCode("helpUsageMap.gml"), "scr_neoconsole_init", 138);
+        Msl.InsertGMLString(ModFiles.GetCode("commandsMap.gml"), "scr_neoconsole_init", 13);
+        Msl.InsertGMLString(ModFiles.GetCode("helpSyntaxMap.gml"), "scr_neoconsole_init", 77);
+        Msl.InsertGMLString(ModFiles.GetCode("helpUsageMap.gml"), "scr_neoconsole_init", 138);
     }
 }
